423a10f71bbed2e1f9b91bb86700cb2ac0f6e212
HoangREALER <hoangle1742002@google.com>
2023-05-09 17:49:47 +0700

    Busy day

--- src/config.php
+++ src/config.php
@@ -5,6 +5,6 @@ $DB = [
         'host' => getenv('MYSQL_HOST') ? getenv('MYSQL_HOST') : 'db',
         'database' => getenv('MYSQL_DATABASE') ? getenv('MYSQL_DATABASE') : 'OnlineEnterprise',
         'user' =>  getenv('MYSQL_USER') ? getenv('MYSQL_USER') : 'hmm',
-        'password' =>  getenv('MYSQL_PASSWORD') ? getenv('MYSQL_PASSWORD') : 'webAss123'
+        'password' =>  getenv('MYSQL_PASSWORD') ? getenv('MYSQL_PASSWORD') : 'webAss@123'
     ]
 ];
